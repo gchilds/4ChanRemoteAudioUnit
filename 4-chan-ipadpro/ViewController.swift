049d8912e9439d46b4879f6879b3150244b9f3e0
Gordon Childs <gordon.childs@gmail.com>
2015-12-09 12:38:20 +1100

    Initial Commit

---
+++ 4-chan-ipadpro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  4-chan-ipadpro
+//
+//  Created by Gordon Childs on 9/12/2015.
+//  Copyright © 2015 Gordon Childs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
